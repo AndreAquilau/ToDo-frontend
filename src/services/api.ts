f25950ad29aa02667b4c11a4e845cb4c8a9d3ed0
AndreAquilau <andre.aquilau@outlook.com>
2020-10-20 22:52:55 -0300

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.URL_API || 'http://localhost:5000',
+  baseURL: process.env.URL_API || 'https://todo-back-end-app.herokuapp.com',
 });
 
 export default api;
